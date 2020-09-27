f488efdbc6bcd4de61044f96594dfa3545019159
Mathieu Havel <mhavel@users.noreply.github.com>
2020-09-27 18:36:15 +0200

    Update params.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/pkg/params.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/pkg/params.py
@@ -8,6 +8,10 @@ Parameters intended to be used / modified by the end-user (so that there should 
 Modify these parameters in your package's root `__init__.py` file, before loading `pkg.config`
 """
 
-DEFAULT_CONFIG_FILE = '{{cookiecutter.app_name}}.yml'
-BASE_CONFIG = None
+from .data import interpret_resource
 
+
+DEFAULT_CONFIG_FILE = '{{cookiecutter.app_name}}.yml'
+BASE_CONFIG = None   # eg with package's resource file: interpret_resource('pkg/data/myconfig.yml')
+INSTALL_CONFIG_FILE = '{{cookiecutter.app_name}}.yml'   # use None to disable use of installed data config file, or change the target's name
+CREATE_USER_CONFIG_IF_NONE = True
