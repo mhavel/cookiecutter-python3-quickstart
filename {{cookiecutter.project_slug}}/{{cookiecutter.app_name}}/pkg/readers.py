b3aa2ec712e131e21b6ea78284ea1975e1c0eab7
Mathieu Havel <mhavel@users.noreply.github.com>
2020-10-07 21:32:17 +0200

    Update readers.py
    - Use package's utils.yaml module (to select automatically the best library available)
    - added `readers` option in `interpret_file`
    - added `reader` option in `read_file`

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/pkg/readers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/pkg/readers.py
@@ -7,7 +7,9 @@ Functions to read files, Agnostic to where the file's is
 
 from pathlib import Path
 import srsly
-import yaml
+# import yaml
+
+from ..utils import yaml
 
 
 _TEXT_EXT = (
@@ -25,7 +27,7 @@ _TEXT_EXT = (
 
 
 # YAML C loader only available when libyaml is installed
-YamlLoader = getattr(yaml, 'CLoader', yaml.Loader)
+# YamlLoader = getattr(yaml, 'CLoader', yaml.Loader)
 
 
 def read_yaml(x):
@@ -42,10 +44,11 @@ def read_yaml(x):
                 x = p.read_bytes()
             else:
                 raise ValueError(f'this is not a valid YAML file path: {p}')
-    return yaml.load(x, Loader=YamlLoader)
+    # return yaml.load(x, Loader=YamlLoader)
+    return yaml.load(x)
 
 
-def read_file(path, as_bytes=None, encoding='utf-8', loader=None, on_missing=None):
+def read_file(path, as_bytes=None, encoding='utf-8', loader=None, reader=None, on_missing=None):
     """Read a file's content either as a simple string / bytes, or using the given loader function which takes the
     read str/bytes as input"""
     p = Path(path).expanduser().resolve()
@@ -65,27 +68,37 @@ def read_file(path, as_bytes=None, encoding='utf-8', loader=None, on_missing=Non
     if as_bytes is None:
         sl = p.suffix.lower()
         as_bytes = sl not in _TEXT_EXT
-    if as_bytes:
+    if reader is not None:
+        assert callable(reader)
+        dat = reader(p)
+    elif as_bytes:
         dat = p.read_bytes()
     else:
         dat = p.read_text(encoding=encoding)
 
-    if loader is not None:
+    if loader is not None and reader is None:
         return loader(dat)
     else:
         return dat
 
 
-def interpret_file(path, encoding='utf-8'):
+def interpret_file(path, encoding='utf-8', readers: dict=None):
     """Read a file's using the proper loader from the extension"""
     path = Path(path).expanduser().resolve()
     s = path.suffix.lower()
-    if s == '.json':
+    if readers is None:
+        readers = {}
+    if s in readers:
+        func = readers[s]
+        assert callable(func)
+        return func(path)
+    elif s == '.json':
         return srsly.read_json(path)
     elif s == '.jsonl':
         return srsly.read_jsonl(path)
     elif s in ('.yml', '.yaml'):
-        return yaml.load(path.read_bytes(), Loader=YamlLoader)
+        # return yaml.load(path.read_bytes(), Loader=YamlLoader)
+        return yaml.load(path.read_bytes())
     elif s in ('.pkl', '.bin', '.pickle'):
         return srsly.pickle_loads(path.read_text(encoding=encoding))
     elif s not in _TEXT_EXT:
