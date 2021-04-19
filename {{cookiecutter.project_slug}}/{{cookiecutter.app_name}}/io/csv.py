96063d316ff6dbbf4f2629f77e15805ff3293c23
Mathieu Havel <mhavel@users.noreply.github.com>
2021-04-19 12:08:13 +0200

    Update csv.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/io/csv.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/io/csv.py
@@ -9,42 +9,56 @@ from pathlib import Path
 
 import pandas as pd
 
-from ..utils.logging import get_logger
+from ..utils.logging import get_sub_logger
+from .common import check_suffix_, check_type_
 
 
-logger = get_logger('{pkg}.io.csv')
+logger = get_sub_logger('io.csv')
 
 
+NAME = 'csv'
 VALID_EXTENSIONS = ('.csv', )
 DEFAULT_EXTENSION = '.csv'
+DEFAULT_READ_OPTS = dict(engine='c', sep=';', low_memory=False, encoding='utf-8')
+DEFAULT_WRITE_OPTS = dict(sep=';', index=False, encoding='utf-8')
+VALID_TYPES = (pd.DataFrame, pd.Series)
 
 
-def check_suffix(path: Union[Path, str]):
-    path = Path(path)
-    if path.suffix.lower() not in VALID_EXTENSIONS:
-        path = path.with_name(path.name + DEFAULT_EXTENSION)
-    return path
+def check_suffix(path: Union[Path, str], raise_error: bool=False):
+    return check_suffix_(path, VALID_EXTENSIONS, DEFAULT_EXTENSION, raise_error=raise_error)
 
 
-def write(data: pd.DataFrame, path: Union[str, Path], sep=';'):
+def check_type(data, raise_error=True):
+    return check_type_(data, NAME, VALID_TYPES, raise_error=raise_error)
+
+
+def write(data: pd.DataFrame, path: Union[str, Path], fix_suffix: bool=True, **kw):
     """
     Write dataframe to CSV
     """
-    path = check_suffix(path)
+    check_type(data)
+    path = check_suffix(path, raise_error=not fix_suffix)
 
     if not path.parent.is_dir():
         path.parent.mkdir(parents=True)
 
-    data.to_csv(path, index=False, sep=sep, encoding='utf-8')
+    opts = dict(DEFAULT_WRITE_OPTS, **kw)
+
+    data.to_csv(path, **opts)
     logger.info(f'data exported to CSV file: {path}')
 
     return path
 
 
-def read(path: Union[str, Path], sep=';', engine='c', encoding='utf-8'):
-    path = check_suffix(path)
+def read(path: Union[str, Path], fix_suffix: bool=True, **kw):
+    """
+    Read dataframe from CSV
+    """
+    path = check_suffix(path, raise_error=not fix_suffix)
 
-    df = pd.read_csv(path, sep=sep, engine=engine, encoding=encoding, low_memory=False)
+    opts = dict(DEFAULT_READ_OPTS, **kw)
+
+    df = pd.read_csv(path, **opts)
     logger.info(f'CSV data loaded from: {path}')
 
     return df
