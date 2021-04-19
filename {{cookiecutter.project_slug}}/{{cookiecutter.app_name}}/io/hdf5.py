066dcbc727099efc476a7d9ae76c974b0535819d
Mathieu Havel <mhavel@users.noreply.github.com>
2021-04-19 12:08:50 +0200

    Update hdf5.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/io/hdf5.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/io/hdf5.py
@@ -10,69 +10,65 @@ from pathlib import Path
 import pandas as pd
 
 from ..utils.serialization import pickle_protocol as set_pickle_protocol
-from ..utils.logging import get_logger
+from ..utils.logging import get_sub_logger
+from .common import check_suffix_, check_type_
 
 
-logger = get_logger('{pkg}.io.hdf5')
+logger = get_sub_logger('io.hdf5')
 
 
-VALID_EXTENSIONS = ('.hdf', '.hdf5')
+NAME = 'hdf5'
+VALID_EXTENSIONS = ('.hdf', '.hdf5', '.h5')
 DEFAULT_EXTENSION = '.hdf5'
-
-DEFAULT_COMPLEVEL = 6
-DEFAULT_COMPLIB = 'blosc:zlib'
-
-
-def check_suffix(path: Union[Path, str]):
-    path = Path(path)
-    if path.suffix.lower() not in VALID_EXTENSIONS:
-        path = path.with_name(path.name + DEFAULT_EXTENSION)
-    return path
+DEFAULT_READ_OPTS = {}
+DEFAULT_WRITE_OPTS = dict(complevel=6, complib='blosc:zlib', mode='w', key='df')
+VALID_TYPES = (pd.DataFrame, pd.Series)
 
 
-def write(data: pd.DataFrame, path: Union[str, Path], mode='w', compression=True, pickle_protocol=None):
+def check_suffix(path: Union[Path, str], raise_error: bool=False):
+    return check_suffix_(path, VALID_EXTENSIONS, DEFAULT_EXTENSION, raise_error=raise_error)
+
+
+def check_type(data, raise_error=True):
+    return check_type_(data, NAME, VALID_TYPES, raise_error=raise_error)
+
+
+def write(data: pd.DataFrame, path: Union[str, Path], pickle_protocol=None, fix_suffix: bool=True, **kw):
     """
     Write dataframe to HDF5
     """
-    path = check_suffix(path)
+    check_type(data)
+    path = check_suffix(path, raise_error=not fix_suffix)
 
     if not path.parent.is_dir():
         path.parent.mkdir(parents=True)
 
-    if compression:
-        if isinstance(compression, bool):
-            complevel = DEFAULT_COMPLEVEL
-            complib = DEFAULT_COMPLIB
-        elif isinstance(int):
-            complevel = compression
-            complib = DEFAULT_COMPLIB
-        elif isinstance(str):
-            complevel = DEFAULT_COMPLEVEL
-            complib = compression
-        else:
-            assert isinstance(compression, (list, tuple))
-            complevel, complib = compression[:2]
-    else:
-        complevel = complib = None
-
+    opts = dict(DEFAULT_WRITE_OPTS, **kw)
     if pickle_protocol is not None:
         with set_pickle_protocol(pickle_protocol):
-            data.to_hdf(path, key=path.stem, mode=mode, complevel=complevel, complib=complib)
+            data.to_hdf(path, key=path.stem, **opts)
     else:
-        data.to_hdf(path, key=path.stem, mode=mode, complevel=complevel, complib=complib)
+        data.to_hdf(path, key=path.stem, **opts)
+    
     logger.info(f'data dumped to HDF5 file: {path}')
 
     return path
 
 
-def read(path: Union[str, Path], pickle_protocol=None):
-    path = check_suffix(path)
+def read(path: Union[str, Path], pickle_protocol=None, fix_suffix: bool=False, **kw):
+    """
+    Read dataframe from HDF-5
+    """
+    path = check_suffix(path, raise_error=not fix_suffix)
+
+    opts = dict(DEFAULT_READ_OPTS, **kw)
 
     if pickle_protocol is not None:
         with set_pickle_protocol(pickle_protocol):
-            df = pd.read_hdf(path)
+            df = pd.read_hdf(path, **opts)
     else:
-        df = pd.read_hdf(path)
+        df = pd.read_hdf(path, **opts)
+    
     logger.info(f'HDF5 data loaded from: {path}')
 
     return df
