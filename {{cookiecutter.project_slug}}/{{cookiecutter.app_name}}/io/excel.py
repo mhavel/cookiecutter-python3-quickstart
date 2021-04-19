36261481ff7dddd7afa229223389892f28f45e9b
Mathieu Havel <mhavel@users.noreply.github.com>
2021-04-19 12:08:32 +0200

    Update excel.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/io/excel.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/io/excel.py
@@ -9,42 +9,56 @@ from pathlib import Path
 
 import pandas as pd
 
-from ..utils.logging import get_logger
+from ..utils.logging import get_sub_logger
+from .common import check_suffix_, check_type_
 
 
-logger = get_logger('{pkg}.io.excel')
+logger = get_sub_logger('io.excel')
 
 
+NAME = 'excel'
 VALID_EXTENSIONS = ('.xls', '.xlsx')
 DEFAULT_EXTENSION = '.xlsx'
+DEFAULT_READ_OPTS = {}
+DEFAULT_WRITE_OPTS = dict(index=False)
+VALID_TYPES = (pd.DataFrame, pd.Series)
 
 
-def check_suffix(path: Union[Path, str]):
-    path = Path(path)
-    if path.suffix.lower() not in VALID_EXTENSIONS:
-        path = path.with_name(path.name + DEFAULT_EXTENSION)
-    return path
+def check_suffix(path: Union[Path, str], raise_error: bool=False):
+    return check_suffix_(path, VALID_EXTENSIONS, DEFAULT_EXTENSION, raise_error=raise_error)
 
 
-def write(data: pd.DataFrame, path: Union[str, Path], sheet_name='Sheet1'):
+def check_type(data, raise_error=True):
+    return check_type_(data, NAME, VALID_TYPES, raise_error=raise_error)
+
+
+def write(data: pd.DataFrame, path: Union[str, Path], fix_suffix: bool=True, **kw):
     """
     Write dataframe to Excel
     """
-    path = check_suffix(path)
+    check_type(data)
+    path = check_suffix(path, raise_error=not fix_suffix)
 
     if not path.parent.is_dir():
         path.parent.mkdir(parents=True)
 
-    data.to_excel(path, index=False, sheet_name=sheet_name)
+    opts = dict(DEFAULT_WRITE_OPTS, **kw)
+
+    data.to_excel(path, **opts)
     logger.info(f'data exported to Excel file: {path}')
 
     return path
 
 
-def read(path: Union[str, Path], sheet_name=None):
-    path = check_suffix(path)
+def read(path: Union[str, Path], fix_suffix: bool=True, **kw):
+    """
+    Read dataframe from Excel
+    """
+    path = check_suffix(path, raise_error=not fix_suffix)
 
-    df = pd.read_excel(path, sheet_name=sheet_name)
+    opts = dict(DEFAULT_READ_OPTS, **kw)
+
+    df = pd.read_excel(path, **opts)
     logger.info(f'Excel data loaded from: {path}')
 
     return df
