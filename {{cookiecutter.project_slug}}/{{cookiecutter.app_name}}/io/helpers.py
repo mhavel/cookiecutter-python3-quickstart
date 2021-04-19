5d0915fd09ba14ec79db7ebe1b84a01e0a8e616c
Mathieu Havel <mhavel@users.noreply.github.com>
2021-04-19 12:23:56 +0200

    Update helpers.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/io/helpers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/io/helpers.py
@@ -83,6 +83,8 @@ def path_and_skey(path: Union[str, Path]):
 def read(path: Union[str, Path], **kw):
     p, s = path_and_skey(path)
     m = get_module(s)
+    if p.is_file():
+        raise FileNotFoundError(p)
     return m.read(p, fix_suffix=False, **kw)
 
 
